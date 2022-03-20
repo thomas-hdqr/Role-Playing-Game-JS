dc5f208f47c02e0dd58762726a6e075cd9665db3
Thomas Haudiquer <thomashaudiquer@gmail.comm>
2022-03-20 17:57:53 +0000

    commit

--- index.js
+++ index.js
@@ -13,7 +13,17 @@ function attack() {
 }
 
 function endGame() {
-    console.log('the game is over')
+    const endMessage = wizard.health === 0 && orc.health === 0 ?
+    "No victors - all creatures are dead" :
+    wizard.health > 0 ? "The Wizard Wins" :
+    "The Orc is Victorious"
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    document.body.innerHTML = 
+        `<div class="end-game">
+        <h2>Game Over</h2>
+        <h3>${endMessage}</h3>
+        <p class="end-emoji">${endEmoji}</p>
+        </div>` 
 }
 
 
